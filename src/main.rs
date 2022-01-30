ee93bc3c8917139491e607cbc6bfa576cfa31664
grostaco <grostaco@gmail.com>
2022-01-30 10:37:32 +0700

    refactor: empty out main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,1 @@
-mod discord;
-mod script;
-
-use script::Config;
-
-fn main() {
-    let cfg = Config::from_file("resources/characters.conf").unwrap();
-
-    println!("{:?}", cfg);
-}
+fn main() {}
