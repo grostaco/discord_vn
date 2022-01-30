df99db19cc08b0413596119bf4526f52e4bb9f43
grostaco <grostaco@gmail.com>
2022-01-30 15:16:10 +0700

    feat: implement JumpDirective

--- src/engine/directives.rs
+++ src/engine/directives.rs
@@ -1,10 +1,36 @@
-trait Directive {
-    fn want_prefix(prefix: &str) -> bool;
+use super::Script;
+
+pub trait Directive {
     fn from_context(ctx: &str) -> Self;
 }
 
-struct JumpDirective {}
+#[derive(Debug)]
+pub struct JumpDirective {
+    pub choices: Option<(String, String)>,
+    pub endpoint: Script,
+}
 
-struct SpriteDirective {}
+impl Directive for JumpDirective {
+    /// Return a jump directive from context
+    /// "A", "B", endpoint.script to jump to endpoint.script if A is taken or
+    /// endpoint.script
+    fn from_context(ctx: &str) -> Self {
+        let ctx = ctx.split(",").take(3).collect::<Vec<_>>();
+        let (choices, endpoint) = match &ctx[..] {
+            [a, b, endpoint] => (Some((a.to_string(), b.to_string())), endpoint),
+            [endpoint] => (None, endpoint),
+            _ => panic!("A jump directive cannot be empty"),
+        };
+        Self {
+            choices,
+            endpoint: Script::from_file(endpoint).expect("Unable to load script file"),
+        }
+    }
+}
+
+struct SpriteDirective {
+    location: Option<String>,
+    sprite_type: Option<String>,
+}
 
 struct EndingDirective {}
