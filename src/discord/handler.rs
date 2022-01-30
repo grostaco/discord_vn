6a0e1c8f35fd29ae5cfa871da4cc12566de95fed
grostaco <grostaco@gmail.com>
2022-01-30 20:39:36 +0700

    feat: add handler

--- src/discord/handler.rs
+++ src/discord/handler.rs
@@ -1,6 +1,35 @@
-use serenity::{async_trait, client::EventHandler};
+use std::env;
+
+use serenity::{
+    async_trait,
+    client::{Context, EventHandler},
+    model::{gateway::Ready, id::GuildId, interactions::Interaction},
+};
 
 pub struct Handler;
 
 #[async_trait]
-impl EventHandler for Handler {}
+impl EventHandler for Handler {
+    async fn interaction_create(&self, _ctx: Context, _interaction: Interaction) {}
+
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+
+        let guild = GuildId(
+            env::var("GUILD_ID")
+                .expect("GUILD_ID not set")
+                .parse()
+                .expect("GUILD_ID must be an integer"),
+        );
+        let guild_command = GuildId::set_application_commands(&guild, &ctx.http, |commands| {
+            commands.create_application_command(|command| command.name("begin").description(":>"))
+        })
+        .await
+        .expect("Unable to add guild commands");
+
+        println!(
+            "The bot has registered the following guild slash commands {:#?}",
+            guild_command,
+        );
+    }
+}
