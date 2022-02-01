3d58e64a011d14cc0691b53f18460124a77523e7
grostaco <grostaco@gmail.com>
2022-02-01 17:11:40 +0700

    feat: add ParseError

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,9 +1,11 @@
 mod config;
 mod directives;
 mod engine;
+mod errors;
 mod script;
 
 pub use config::Config;
 pub use directives::*;
 pub use engine::Engine;
+pub use errors::*;
 pub use script::{Script, ScriptContext, ScriptDirective};
