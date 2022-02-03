3ab1f298b96dc9a4074f2c0a799dc9503351aeed
grostaco <grostaco@gmail.com>
2022-02-03 22:05:06 +0700

    feat: add error handling for script errors

--- src/bin/engine.rs
+++ src/bin/engine.rs
@@ -40,72 +40,80 @@ fn main() {
         },
     };
     println!("[*] Engine initializing, searching for script.txt");
-    if let Ok(mut engine) = Engine::from_file("script.txt", &scene) {
-        println!("[*] Engine initialized. Rendering...");
-        println!("[!] It should be noted that if there are conditional jumps in the script, you will be prompted.");
-        while let Some(ctx) = engine.current() {
-            let mut choice = false;
-            match ctx {
-                ScriptContext::Dialogue(dialogue) => {
-                    println!(
-                        "[*] Rendering dialogue: \"{}\"",
-                        dialogue.dialogues.iter().fold(String::new(), |a, b| a + b)
-                    )
-                }
-                ScriptContext::Directive(directive) => match directive {
-                    ScriptDirective::LoadBG(loadbg) => {
-                        println!("[*] Loading background {}", loadbg.bg_path);
+    match Engine::from_file("script.txt", &scene) {
+        Ok(mut engine) => {
+            println!("[*] Engine initialized. Rendering...");
+            println!("[!] It should be noted that if there are conditional jumps in the script, you will be prompted.");
+            while let Some(ctx) = engine.current() {
+                let mut choice = false;
+                match ctx {
+                    ScriptContext::Dialogue(dialogue) => {
+                        println!(
+                            "[*] Rendering dialogue: \"{}\"",
+                            dialogue.dialogues.iter().fold(String::new(), |a, b| a + b)
+                        )
                     }
-                    ScriptDirective::Jump(jump) => match &jump.choices {
-                        Some((a, b)) => {
-                            let mut buf = String::new();
-                            loop {
-                                println!(
+                    ScriptContext::Directive(directive) => match directive {
+                        ScriptDirective::LoadBG(loadbg) => {
+                            println!("[*] Loading background {}", loadbg.bg_path);
+                        }
+                        ScriptDirective::Jump(jump) => match &jump.choices {
+                            Some((a, b)) => {
+                                let mut buf = String::new();
+                                loop {
+                                    println!(
                                     "[?] A conditional choice was found, choose 1 or 2.\n ├──[1] {}\n └──[2] {}",
                                     a, b
                                 );
-                                io::stdin().read_line(&mut buf).unwrap();
-                                match buf.trim().parse::<u64>() {
-                                    Ok(num) => match num {
-                                        1 => {
-                                            choice = true;
-                                            break;
-                                        }
-                                        2 => {
-                                            choice = false;
-                                            break;
-                                        }
-                                        _ => println!(
+                                    io::stdin().read_line(&mut buf).unwrap();
+                                    match buf.trim().parse::<u64>() {
+                                        Ok(num) => match num {
+                                            1 => {
+                                                choice = true;
+                                                break;
+                                            }
+                                            2 => {
+                                                choice = false;
+                                                break;
+                                            }
+                                            _ => println!(
                                             "[!] The choice must be either 1 or 2. Reprompting."
                                         ),
-                                    },
-                                    Err(_) => {
-                                        println!("[!] The choice number must be an integer. Reprompting.");
+                                        },
+                                        Err(_) => {
+                                            println!("[!] The choice number must be an integer. Reprompting.");
+                                        }
                                     }
+                                    buf.clear();
                                 }
-                                buf.clear();
+                            }
+                            None => println!("[*] Jumping to {}", jump.endpoint.name),
+                        },
+                        ScriptDirective::Sprite(sprite) => {
+                            if let Some(sprite_path) = &sprite.sprite_path {
+                                println!("[*] Loading sprite {}", sprite_path)
+                            } else {
+                                println!("[*] Unloading sprite {}", sprite.name)
                             }
                         }
-                        None => println!("[*] Jumping to {}", jump.endpoint.name),
                     },
-                    ScriptDirective::Sprite(sprite) => {
-                        println!("[*] Loading sprite {}", sprite.sprite_path)
-                    }
-                },
-            };
-            engine.render_to(&format!(
-                "resources/render/{}_{}.png",
-                engine
-                    .script
-                    .name
-                    .get(engine.script.name.rfind("/").unwrap_or(0)..)
-                    .unwrap(),
-                engine.iscript
-            ));
-            engine.next(choice);
+                };
+                engine.render_to(&format!(
+                    "resources/render/{}_{}.png",
+                    engine
+                        .script
+                        .name
+                        .get(engine.script.name.rfind("/").unwrap_or(0)..)
+                        .unwrap(),
+                    engine.iscript
+                ));
+                engine.next(choice);
+            }
         }
-    } else {
-        println!("[!] Cannot find scripts.txt. Aborting.");
-        exit(0);
+        Err(e) => {
+            println!("[!] {}", e);
+            println!("[!] Cannot initialize scripts.txt. Aborting.");
+            exit(0);
+        }
     }
 }
