3a0ee729e62befb0dc7b9993df88bf003671e195
grostaco <grostaco@gmail.com>
2022-02-05 14:47:11 +0700

    feat: add voice channels support

---
+++ src/discord/voice.rs
@@ -0,0 +1,96 @@
+use serenity::{
+    client::Context, http::Http, json::Value,
+    model::interactions::application_command::ApplicationCommandInteraction,
+};
+use songbird::input::restartable::Restartable;
+use std::sync::Arc;
+
+use super::PlayError;
+
+macro_rules! cast {
+    ($target: expr, $pat: path) => {{
+        if let $pat(a) = $target {
+            a
+        } else {
+            panic!("mismatch variant when cast to {}", stringify!($pat))
+        }
+    }};
+}
+
+macro_rules! cast_opt {
+    ($target: expr, $pat: path) => {
+        $target.map_or(None, |value| Some(cast!(value, $pat)))
+    };
+}
+
+pub async fn play_url(
+    ctx: &Context,
+    channel_id: u64,
+    guild_id: u64,
+    url: &str,
+) -> Result<(), PlayError> {
+    if !url.starts_with("http") {
+        return Err(PlayError::InvalidURL(url.to_string()));
+    }
+
+    let manager = songbird::get(ctx)
+        .await
+        .ok_or(PlayError::SongbirdError)?
+        .clone();
+    let _result = manager.join(guild_id, channel_id).await;
+
+    if let Some(handler_lock) = manager.get(guild_id) {
+        let mut handler = handler_lock.lock().await;
+
+        let source = match Restartable::ytdl(url.trim().to_owned(), true).await {
+            Ok(source) => source,
+            Err(why) => Err(PlayError::InputError(why))?,
+        };
+
+        let handle = handler.play_only_source(source.into());
+        handle.enable_loop().expect("Cannot enable loop");
+    }
+
+    Ok(())
+}
+
+pub async fn play(
+    http: &Arc<Http>,
+    ctx: &Context,
+    aci: ApplicationCommandInteraction,
+) -> Result<(), PlayError> {
+    let url = aci
+        .data
+        .options
+        .iter()
+        .find(|opt| opt.name == "url")
+        .map(|opt| cast_opt!(opt.value.as_ref(), Value::String).unwrap())
+        .unwrap();
+
+    if !url.starts_with("http") {
+        return Err(PlayError::InvalidURL(url.to_string()));
+    }
+
+    aci.create_interaction_response(http, |r| r.interaction_response_data(|d| d.content("A")))
+        .await
+        .map_err(|e| PlayError::SerenityError(e))?;
+
+    let manager = songbird::get(ctx)
+        .await
+        .ok_or(PlayError::SongbirdError)?
+        .clone();
+    let _result = manager.join(887345509990285312, 887345509990285316).await;
+
+    if let Some(handler_lock) = manager.get(887345509990285312) {
+        let mut handler = handler_lock.lock().await;
+
+        let source = match Restartable::ytdl(url.trim().to_owned(), false).await {
+            Ok(source) => source,
+            Err(why) => Err(PlayError::InputError(why))?,
+        };
+        let handle = handler.play_only_source(source.into());
+        handle.enable_loop().expect("Cannot enable loop");
+    }
+
+    Ok(())
+}
