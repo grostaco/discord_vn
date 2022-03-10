303e6c5f2539323db3a22e4e0e581f915ccb2a58
grostaco <grostaco@gmail.com>
2022-03-10 09:43:03 +0700

    feat: test

---
+++ examples/arc.rs
@@ -0,0 +1,114 @@
+use std::mem::swap;
+
+use image::{DynamicImage, GenericImage, GenericImageView};
+
+// taken from https://docs.rs/imageproc/latest/src/imageproc/drawing/line.rs.html#170-184
+struct BresenhamLineIter {
+    dx: f32,
+    dy: f32,
+    x: i32,
+    y: i32,
+    error: f32,
+    end_x: i32,
+    is_steep: bool,
+    y_step: i32,
+}
+
+impl BresenhamLineIter {
+    pub fn new(start: (f32, f32), end: (f32, f32)) -> BresenhamLineIter {
+        let (mut x0, mut y0) = start;
+        let (mut x1, mut y1) = end;
+
+        let is_steep = (y1 - y0).abs() > (x1 - x0).abs();
+        if is_steep {
+            swap(&mut x0, &mut y0);
+            swap(&mut x1, &mut y1);
+        }
+
+        if x0 > x1 {
+            swap(&mut x0, &mut x1);
+            swap(&mut y0, &mut y1);
+        }
+
+        let dx = x1 - x0;
+
+        Self {
+            dx,
+            dy: (y1 - y0).abs(),
+            x: x0 as i32,
+            y: y0 as i32,
+            error: dx / 2f32,
+            end_x: x1 as i32,
+            is_steep,
+            y_step: if y0 < y1 { 1 } else { -1 },
+        }
+    }
+}
+
+impl Iterator for BresenhamLineIter {
+    type Item = (i32, i32);
+
+    fn next(&mut self) -> Option<(i32, i32)> {
+        if self.x > self.end_x {
+            None
+        } else {
+            let ret = if self.is_steep {
+                (self.y, self.x)
+            } else {
+                (self.x, self.y)
+            };
+
+            self.x += 1;
+            self.error -= self.dy;
+            if self.error < 0f32 {
+                self.y += self.y_step;
+                self.error += self.dx;
+            }
+
+            Some(ret)
+        }
+    }
+}
+
+fn draw_line_segment(image: &mut DynamicImage, start: (f32, f32), end: (f32, f32)) {
+    let (width, height) = image.dimensions();
+    let line_iterator = BresenhamLineIter::new(start, end);
+
+    for point in line_iterator {
+        let (x, y) = point;
+        if x >= 0 && x < width as i32 && y >= 0 && y <= height as i32 {
+            image.put_pixel(x as u32, y as u32, [255, 255, 255, 255].into())
+        }
+    }
+}
+
+fn main() {
+    let mut img = DynamicImage::new_rgba8(640, 480);
+
+    let mut x = 0i32;
+    let mut y = 30;
+    let mut p = 1 - y;
+
+    let xc = 50;
+    let yc = 50;
+
+    while x <= y {
+        draw_line_segment(
+            &mut img,
+            (xc as f32, (yc + y) as f32),
+            ((x + xc) as f32, (y + yc) as f32),
+        );
+        //img.put_pixel(x as u32 + xc, y as u32 + yc, [255, 255, 255, 255].into());
+        //img.put_pixel(y as u32 + xc, x as u32 + xc, [255, 255, 255, 255].into());
+
+        if p < 0 {
+            p += 2 * x + 1;
+        } else {
+            y -= 1;
+            p += 2 * (x - y) + 1;
+        }
+        x += 1;
+    }
+
+    img.save("out.png").unwrap();
+}
